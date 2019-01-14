feacf93d531e6299d8d818c0d8dd4414d01f1b0b
Ken Crites <kenrcrites@gmail.com>
2019-01-14 23:47:55 +0100

    changes to server.js to utilize postgresql

--- server.js
+++ server.js
@@ -76,29 +76,34 @@ app.post('/register', (req, res) => {
 	const { email, name, password} = req.body;
 
 	
-	db('users').insert({
+	db('users')
+	.returning('*')
+	.insert({
 		email: email,
 		name: name,
 		joined: new Date()
-	}).then(console.log);
+	}).then(user => {
+		res.json(user[0]);
+	})
+	.catch(err => res.status(400).json('Unable to register'));
 
-	res.json(database.users[database.users.length-1]);
+	
 })
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
 
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			return res.json(user);
-		}
-		
-	})
-	if(!found){
-		res.status(400).json('User not found');
-	}
+db.select('*').from('users').where({id})
+	.then(user => {
+		if (user.length) {
+	res.json(user[0]);
+} else {
+	res.status(400).json('Not Found')
+}
+}).catch(err => res.status(400).json('Error getting user'))
+	// if(!found){
+	// 	res.status(400).json('User not found');
+	// }
 
 })
 
