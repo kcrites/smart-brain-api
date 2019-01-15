2db66cfdc72ab1e78d3cc36afa65135cc70bfc13
Ken Crites <kenrcrites@gmail.com>
2019-01-15 14:13:31 +0100

    End of development, pre-code review

--- server.js
+++ server.js
@@ -22,72 +22,57 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const findUser = () => {
-	const { id } = req.params;
-	let found = false;
 
-	database.users.forEach(user => {
-		if(user.id === id){
-			found = true;
-			return res.json(user);
-		}
-		
-	})
-	if(!found){
-		res.status(400).json('User not found');
-	}
-};
-
-const database = {
-	users: [
-	{
-		id: '123',
-		name: 'John',
-		email: 'john@gmail.com',
-		password: 'cookies',
-		entries: 0,
-		joined: new Date()
-	},	{
-		id: '124',
-		name: 'Sally',
-		email: 'Sally@gmail.com',
-		password: 'bananas',
-		entries: 0,
-		joined: new Date()
-	}]
-}
 
 app.get('/', (req, res) => {
 	res.send(database.users);
 })
 
 app.post('/signin', (req, res) => {
-	if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json(database.users[0]);
-
-	}
-	else {
-		res.status(400).json('Error logging in');
-	}
+	db.select('email', 'hash').from('login')
+	.where('email', '=', req.body.email)
+	.then(data => {
+		const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+		if(isValid) {
+			return db.select('*').from('users')
+				.where('email', '=', req.body.email)
+				.then(user => {
+					res.json(user[0])
+				})
+				.catch(err => res.status(400).json('Unable to get user'))
+		} else {
+			res.status(400).json('Wrong credentials');
+			}
+	})
+	.catch(err => res.status(400).json('Wrong credentials'));
 
 })
 
 app.post('/register', (req, res) => {
 	const { email, name, password} = req.body;
-
-	
-	db('users')
-	.returning('*')
-	.insert({
-		email: email,
-		name: name,
-		joined: new Date()
-	}).then(user => {
-		res.json(user[0]);
-	})
-	.catch(err => res.status(400).json('Unable to register'));
-
-	
+	const hash = bcrypt.hashSync(password);
+		db.transaction(trx => {
+			trx.insert({
+				hash: hash,
+				email: email
+			})
+			.into('login')
+			.returning('email')
+			.then(loginEmail => {
+					return trx('users')
+					.returning('*')
+					.insert({
+						email: loginEmail[0],
+						name: name,
+						joined: new Date()
+					}).then(user => {
+						res.json(user[0]);
+					})
+			})
+			.then(trx.commit)
+			.catch(trx.rollback)
+		})	
+		.catch(err => res.status(400).json('Unable to register'));	
 })
 
 app.get('/profile/:id', (req, res) => {
@@ -101,43 +86,27 @@ db.select('*').from('users').where({id})
 	res.status(400).json('Not Found')
 }
 }).catch(err => res.status(400).json('Error getting user'))
-	// if(!found){
-	// 	res.status(400).json('User not found');
-	// }
+
 
 })
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-		let found = false;
+	db('users')
+	.where('id', '=', req.body.id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries =>{
+		res.json(entries);
+	})
+	.catch(err => res.status(400).json('Unable to get entries'))
 
-		database.users.forEach(user => {
-			if(user.id === id){
-				found = true;
-				user.entries++;
-				return res.json(user.entries);
-			}
-			
-		})
-		if(!found){
-			res.status(400).json('User not found');
-		}
+ 	
 })
 
 app.listen(3001, () => {
 	console.log('app is running on port 3001');
 
 })
-/*
-bcrypt.hash("bacon", null, null, function(err, hash) {
-    // Store hash in your password DB.
-});
-
-// Load hash from your password DB.
-bcrypt.compare("bacon", hash, function(err, res) {
-    // res == true
-});
-bcrypt.compare("veggies", hash, function(err, res) {
-    // res = false
-});
-*/
+// Using port 3001 because the front end React server is using port 3000.
+  
